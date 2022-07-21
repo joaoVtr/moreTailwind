8724a0abecab962d692083707eab82d1eb544eaa
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-07-21 01:22:58 -0300

    feat/estados e config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./pages/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azul: {
+          100: "#0038FF",
+          200: "#00389A",
+          300: "#003865",
+        },
+      },
+    },
   },
   plugins: [],
 };
